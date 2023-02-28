3c86ec625995ba9d437ad541c336a6b71f40a74e
martian17 <yakumodanshi@gmail.com>
2023-02-28 17:46:58 +0900

    create utils

---
+++ util.mjs
@@ -0,0 +1,10 @@
+export const intdiv = function(a,b){
+    return Math.floor(a/b);
+};
+
+export const convertEndian = function(num){
+    return (num>>>24&0xff)
+    |(num>>>8&0xff00)
+    |(num<<8&0xff0000)
+    |(num<<24&0xff000000);
+};
