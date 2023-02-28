348a9cdbefdf691778413981dcbb2b98a0d6bf2d
martian17 <yakumodanshi@gmail.com>
2023-02-28 17:38:48 +0900

    modify extraction code to utilize temp and testdata dirs

--- test/extract-chunk.mjs
+++ test/extract-chunk.mjs
@@ -1,11 +1,17 @@
 // extract chunk nbt as binary for use testing in nbt.mjs
 
-import {promises as fs} from "fs"; 
-import {World} from "../index.mjs";
-//const world = new World("./world2");
-const world = new World("/home/yutaro/.minecraft/saves/NBT_TEST");
-const {overworld} = world;
+import {Dimension} from "../dimension.mjs";
+import {execSync} from "child_process";
+import {normalizeObject} from "../ds-js/objutil.mjs";
+import {promises as fs} from "fs";
 
+//prepare ./temp
+execSync(`
+rm -rf ./temp/*
+cp ./testdata/r.0.0.mca ./temp/
+`.trim());
+
+const overworld = new Dimension("./temp"); 
 
 const region = await overworld.getRegion(8,9);
 console.log(`getting chunk data from region ${region.path}.`);
@@ -20,6 +26,6 @@ const chunkbuff = await region.getChunkBuffer(chunkID);
 console.log(`chunk buffer size: ${chunkbuff.length}`);
 console.log(chunkbuff);
 
-await fs.writeFile("out.nbt",chunkbuff);
+await fs.writeFile("./testdata/out.nbt",chunkbuff);
 
 
