4b04cce347064662fe4ac78b20d79b8f3b8f8648
martian17 <yakumodanshi@gmail.com>
2023-02-28 17:18:40 +0900

    bugfix

--- chunk.mjs
+++ chunk.mjs
@@ -7,7 +7,7 @@ const decodeBlockStates = function(block_states = {palette:["DNE"]}){
     if(!("palette" in block_states)){
         throw new Error("palette not present in block states");
     }
-    const palette = block_states.palette;
+    const {data,palette} = block_states;
     if(!("data" in block_states)){
         if(palette.length !== 1){
             console.log(palette);
@@ -15,10 +15,9 @@ const decodeBlockStates = function(block_states = {palette:["DNE"]}){
         }
         return newarr(4096).map(_=>palette[0]);
     }
-    const {data,palette} = block_states;
     const bitDepth = Math.ceil(Math.log(palette.length)/Math.log(2));
     const blocks = [];
-    for(let idx of unpackAlignedBitfield_64BE(data,bitDepth,4096)){
+    for(let idx of unpackArray_64BEA(data,bitDepth,4096)){
         if(idx >= palette.length){
             throw new Error(`Block ID ${idx} DNE in palette`);
         }
@@ -32,7 +31,7 @@ const encodeBlockState = function(blockArr){
     const keys = new Map/*<string,int>*/();
     const palette = [];
     const idmap = [];
-
+    
     for(let i = 0; i < 4096; i++){
         const state = blockArr[i];
         let normalized;
@@ -59,7 +58,7 @@ const encodeBlockState = function(blockArr){
         };
     }
     const bitDepth = Math.ceil(Math.log(palette.length)/Math.log(2));
-    const data = PackedAlignedBitfieldBuilder_64BE(bitDepth,4096);
+    const data = new PackedArray_64BEA_Builder(bitDepth,4096);
     for(let i = 0; i < 4096; i++){
         const idx = idmap[i];
         data.set(i,idx);
@@ -78,14 +77,18 @@ class Section{
         this.blockArr = decodeBlockStates(nbt.block_states);
         this.nbt = nbt;
     }
-    getBlockData(x,y,z){
+    modified = false;
+    getBlock(x,y,z){
         return this.blockArr[y*256+z*16+x];
     }
-    setBlockData(x,y,z,data){
+    setBlock(x,y,z,data){
+        this.modified = true;
         this.blockArr[y*256+z*16+x] = data;
     }
     toNBT(){
         const {nbt} = this;
+        if(!this.modified)return nbt;
+        //console.log("encoding");
         nbt.block_states = encodeBlockState(this.blockArr);
         return nbt;
     }
@@ -104,15 +107,17 @@ export class Chunk{
         }
         this.ymin = this.sections[0].y*16;
     }
-    getBlockData(x,y,z){
+    getBlock(x,y,z){
         y = y-this.ymin;
         const sidx = Math.floor(y/16);
-        return this.sections[sidx].getBlockData(x,y%16,z);
+        return this.sections[sidx].getBlock(x,y%16,z);
     }
-    setBlockData(x,y,z,data){
+    modified = false;
+    setBlock(x,y,z,data){
+        this.modified = true;
         y = y-this.ymin;
         const sidx = Math.floor(y/16);
-        return this.sections[sidx].setBlockData(x,y%16,z,data);
+        return this.sections[sidx].setBlock(x,y%16,z,data);
     }
     getBlockID(x,y,z){
     }
@@ -120,10 +125,13 @@ export class Chunk{
     }
     toBuffer(){
         const {sections,nbt} = this;
+        //console.log(nbt.sections.map(s=>s.block_states));
         nbt.sections = sections.map(section=>section.toNBT());
-        return encodeNBT(nbt);
-    }
-    isModified(){
-        return true;
+        //console.log(nbt.sections.map(s=>s.block_states?.palette));
+        //console.log(nbt.sections.map(s=>s.block_states));
+        let res = encodeNBT({"":nbt});
+        //console.log(res);
+        //console.log(decodeNBT(res));
+        return res;
     }
 };
