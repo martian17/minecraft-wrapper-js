61275c5c599dac33da6291135f3186bdcb1bb42e
martian17 <yakumodanshi@gmail.com>
2023-02-28 14:05:22 +0900

    write test for packed array

---
+++ test/packedArrayTest.mjs
@@ -0,0 +1,27 @@
+import {unpackArray_64BEA as unpack, PackedArray_64BEA_Builder as Packer} from "../packedArray64BEA.mjs";
+import {newarr,arreq} from "../ds-js/arrutil.mjs";
+
+const len = 1000;
+const depth = 3;
+
+const source = newarr(len).map(_=>Math.floor(Math.random()*(1<<depth)));
+
+const packer = new Packer(depth,len);
+for(let i = 0; i < len; i++){
+    packer.set(i,source[i]);
+}
+const packed = packer.export();
+
+const unpacked = [];
+for(let val of unpack(packed,depth,len)){
+    unpacked.push(val);
+}
+
+console.log(source);
+console.log(unpacked);
+console.log(packed);
+console.log(arreq(source,unpacked));
+
+
+
+
