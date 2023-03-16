9377351cd0f270ad6caeebc11a2306650646b3e3
martian17 <yakumodanshi@gmail.com>
2023-03-17 03:08:39 +0900

    create test world generation code

--- test/editSingleBlock.mjs
+++ test/editSingleBlock.mjs
@@ -1,22 +1,29 @@
 import {World} from "../index.mjs";
 
 const path = process.argv[2];
-const [x=8,y=-62,z=9] = process.argv.slice(3).map(v=>parseInt(v));
+//const [x=8,y=-61,z=9] = process.argv.slice(3).map(v=>parseInt(v));
 
 if(!path){
     console.log("please specify a path to the save data");
+    process.exit();
 }
 
 const world = new World(path);
 const dim = world.overworld;
 
-await dim.setBlock(x,y,z,{
-    Properties: { rotation: '11' },
-    Name: 'minecraft:wither_skeleton_skull'
-});
+const chunk = await dim.getChunk(0,0);
+
+await dim.setBlock(0,-60,0,{Name:"minecraft:deepslate"});
+await dim.setBlock(1,-60,0,{Name:"minecraft:exposed_cut_copper_stairs"});
+await dim.setBlock(2,-60,0,{Name:"minecraft:wither_skeleton_skull"});
+await dim.setBlock(3,-60,0,{Name:"minecraft:water"});
+
+
+
+console.log(await dim.getBlock(0,-60,0));
 
 console.log("saving");
 await dim.save();
 console.log("saved");
-
+//console.log(`single block edited in ${path} at x:${x} y:${y} z:${z} saved`);
 
