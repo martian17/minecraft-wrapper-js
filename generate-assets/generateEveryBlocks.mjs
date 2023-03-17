d04b194d07e7c87960473ec80bb50607f0f6e038
martian17 <yakumodanshi@gmail.com>
2023-03-18 07:30:42 +0900

    Match the content with the name

--- generate-assets/generateEveryBlocks.mjs
+++ generate-assets/generateEveryBlocks.mjs
@@ -2,54 +2,27 @@ import {World} from "../index.mjs";
 import {promises as fs} from "fs";
 import {execSync} from "child_process";
 
-const jarPath = process.argv[2];
-const worldPath = process.argv[3];
-//const [x=8,y=-61,z=9] = process.argv.slice(3).map(v=>parseInt(v));
+const worldPath = process.argv[2];
 
-if(!jarPath){
-    console.log("pelase provide the jar path and path to the save data");
-    process.exit();
-}
 if(!worldPath){
     console.log("please specify a path to the save data");
     process.exit();
 }
 
-//prepare ./temp
-execSync(`
-rm -rf ./temp
-mkdir ./temp
-cp ${jarPath} ./temp/mc.jar
-mkdir ./temp/jar
-unzip -qq ./temp/mc.jar -d ./temp/jar/
-mv ./temp/jar/assets/minecraft/blockstates ./temp/
-`.trim());
+let states = await fs.readdir("./temp/blockstates/");
+states = states.filter(v=>v.slice(-5) === ".json").map(v=>v.slice(0,-5));
 
-
-
-
-
-
-/*
-const world = new World(path);
+const world = new World(worldPath);
 const dim = world.overworld;
 
-const chunk = await dim.getChunk(0,0);
+for(let i = 0; i < states.length; i++){
+    const bname = states[i];
+    const z = (i%21)*3;
+    const x = Math.floor(i/21)*3;
+    await dim.setBlock(x,-63,z,{Name:`minecraft:${bname}`});
+}
 
-await dim.setBlock(0,-60,0,{Name:"minecraft:deepslate"});
-await dim.setBlock(1,-60,0,{Name:"minecraft:exposed_cut_copper_stairs"});
-await dim.setBlock(2,-60,0,{Name:"minecraft:wither_skeleton_skull"});
-await dim.setBlock(3,-60,0,{Name:"minecraft:water"});
-
-
-
-console.log(await dim.getBlock(0,-60,0));
 
 console.log("saving");
 await dim.save();
 console.log("saved");
-//console.log(`single block edited in ${path} at x:${x} y:${y} z:${z} saved`);
-
-*/
-
-
