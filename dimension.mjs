77a7cfbe9bb923bc8bf14d5b26c6be7685496245
martian17 <yakumodanshi@gmail.com>
2023-04-09 16:19:24 +0900

    Update region constructor

--- dimension.mjs
+++ dimension.mjs
@@ -1,34 +1,31 @@
 import * as Path from "path";
 import {Region} from "./region.mjs";
 import {intdiv} from "./util.mjs";
+import {MultiMap} from "ds-js
+";
 
 export class Dimension{
     // flags
-    generateChunks = false;
+    allowChunkGeneration = false;
 
     constructor(world,path){
         this.world = world;
         this.path = path;
     }
-    regions = new Map;
+    regions = new MultiMap;
     async getRegion(x,z){
-        const {regions} = this;
-        const filename = Path.join(
-            this.path,
-            `r.${intdiv(x,512)}.${intdiv(z,512)}.mca`
-        );
-        if(regions.has(filename)){
-            return regions.get(filename);
-        }else{
-            //.init() will throw error if file DNE
-            const region = await new Region(this,filename).init();
-            regions.set(filename,region);
+        const rx = x%512;
+        const rz = z%512;
+        let region;
+        if(region = this.regions.get(rx,rz)){
             return region;
         }
+        region = await new Region(this,rx,rz).init();
+        this.regions.set(rx,rz,region);
     }
     async getChunk(x,z){
         const region = await this.getRegion(x,z);
-        return await region.getChunk(x%512,z%512);
+        return await region.getChunk(x,z);
     }
     async getBlock(x,y,z){
         const chunk = await this.getChunk(x,z);
