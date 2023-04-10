a660bde4c5ece68b2db75b96b545732ee01c02d0
martian17 <yakumodanshi@gmail.com>
2023-04-10 21:35:38 +0900

    Update API and add colored sector view

--- tools/region-stats.mjs
+++ tools/region-stats.mjs
@@ -1,6 +1,7 @@
 import {World} from "../index.mjs";
 import {intdiv,reverseEndian} from "../util.mjs";
 import {newarr} from "ds-js/arrutil.mjs";
+import chalk from "chalk";
 
 const path = process.argv[2];
 const [x=0,y=0,z=0] = process.argv.slice(3).map(v=>parseInt(v));
@@ -15,61 +16,55 @@ const dim = world.overworld;
 
 const region = await dim.getRegion(x,z);
 
+const {data_header} = region;
+
+console.log("\nRegion occupancy overview");
 const displayRegion = function(){
-    const {i32} = this;
     const grid = newarr(32).map(_=>newarr(32).map(_=>"__"));
     for(let i = 0; i < 1024; i++){
         const x = i%32;
         const z = intdiv(i,32);
-        if(i32[i] !== 0)grid[z][x] = "██";
+        if(data_header[i] !== 0)grid[z][x] = chalk.rgb(x*8,z*8,(31-z)*8)("██");
     }
-    console.log("region overview");
     console.log(grid.map(r=>r.join("")).join("\n"));
-}.bind(region);
+};
 
 displayRegion();
 
-
-const regionstats = [];
+const sectors = new Map;
+let maxIdx = 2;
 
 for(let z = 0; z < 32; z++){
     for(let x = 0; x < 32; x++){
         const id = z*32+x;
-        if(region.i32[id] === 0)
+        if(data_header[id] === 0)
             continue;
-            //throw new Error("Chunk does not exist within the region");
-        let [offset,size,timestamp] = region.getChunkData(id);
-        offset /= 4096;
-        size /= 4096;
-        if(size !== 1)console.log(size,offset);
-
-        // const chunkmeta = reverseEndian(region.i32[id]);
-        // const offset_ = (chunkmeta>>>8);
-        // const size_ = (chunkmeta&0xff);
-        // console.log(size_);
-
-        //console.log((await region.getChunkBuffer(id)).length);
-        regionstats.push([offset,size,timestamp,id,x,z]);
-        if(10 < id && id < 1000)continue;
-        console.log(`x:${x} z:${z} offset:${offset} size:${size} ${new Date(timestamp*1000)}`);
+        const [offset,size] = region.getHeaderBitfield(id);
+        const timestamp = reverseEndian(region.timestamp_header[id]);
+        for(let i = offset; i < offset+size; i++){
+            sectors.set(i,id);
+        }
+        if(offset+size > maxIdx)maxIdx = offset+size;
     }
 }
 
-regionstats.sort((a,b)=>a[0]-b[0]);
+console.log("\nFile sector view (First two sectors are bitfield and timestamp)");
 
-console.log("sorted regions");
-regionstats.map((r,i)=>{
-    const [offset,size,timestamp,id,x,z] = r;
-    if(10 < id && id < 1000)return;
-    console.log(`x:${x} z:${z} offset:${offset} size:${size} ${new Date(timestamp*1000)}`);
-});
+for(let i = 0; i < maxIdx; i++){
+    if(i == 0){
+        process.stdout.write(chalk.white.inverse("BF"));
+    }else if(i === 1){
+        process.stdout.write(chalk.yellow.inverse("TS"));
+    }else if(sectors.has(i)){
+        const id = sectors.get(i);
+        const x = id%32;
+        const z = intdiv(id,32);
+        process.stdout.write(chalk.rgb(x*8,z*8,(31-z)*8)("██"));
+    }else{
+        process.stdout.write("__");
+    }
+    if((i+1)%32 === 0)process.stdout.write("\n");
+}
+console.log("");
 
-
-console.log("sorted by region size");
-regionstats.sort((a,b)=>a[1]-b[1]);
-regionstats.map((r,i)=>{
-    const [offset,size,timestamp,id,x,z] = r;
-    if(10 < id && id < 1000)return;
-    console.log(`x:${x} z:${z} offset:${offset} size:${size} ${new Date(timestamp*1000)}`);
-});
 
