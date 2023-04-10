56df78ff710fac66e9f32e001d1ad7e944a9d98d
martian17 <yakumodanshi@gmail.com>
2023-04-10 22:07:37 +0900

    Add error when path not present, and set defaults to args

--- tools/extractChunkAsRNBT.mjs
+++ tools/extractChunkAsRNBT.mjs
@@ -5,17 +5,22 @@ import {encodeRNBT, decodeRNBT} from "rnbt";
 import {encodeNBT, decodeNBT, NBT_Long} from "nbt.js";
 
 
-let [worldPath, x, y, z, savePath] = process.argv.slice(2);
-[x,y,z] = [x,y,z].map(v=>parseInt(v));
+let [worldPath, savePath, x=0, z=0] = process.argv.slice(2);
+[x,z] = [x,z].map(v=>parseInt(v));
 
+
+if(!worldPath || !savePath){
+    console.log("please specify a path to the save data and output file path");
+    process.exit();
+}
+console.log(`Opening save directory ${worldPath}`);
 const world = new World(worldPath);
 const dim = world.overworld;
+console.log(`Getting the chunk at x=${x} z=${z}`);
 const chunk = await dim.getChunk(x,z);
-console.log(chunk);
 const nbt = {"":chunk.nbt};
 console.log(encodeRNBT(nbt,true,2,true));
-
 const rnbt = encodeRNBT(nbt,true,2,false);
 await fs.writeFile(savePath,rnbt);
-
+console.log(`Saved rnbt to ${savePath}`);
 
