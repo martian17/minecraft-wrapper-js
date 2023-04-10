c419c8839179c5aa8df63a5250d1eb9ec15e5b17
martian17 <yakumodanshi@gmail.com>
2023-04-10 22:00:30 +0900

    Set defaults and display error when filename not present

--- tools/get-block.mjs
+++ tools/get-block.mjs
@@ -5,11 +5,17 @@ import {encodeRNBT, decodeRNBT} from "rnbt";
 import {encodeNBT, decodeNBT, NBT_Long} from "nbt.js";
 
 
-let [worldPath, x, y, z] = process.argv.slice(2);
+let [path, x=0, y=-64, z=0] = process.argv.slice(2);
 [x,y,z] = [x,y,z].map(v=>parseInt(v));
 
-const world = new World(worldPath);
-const dim = world.overworld;
+if(!path){
+    console.log("please specify a path to the save data");
+    process.exit();
+}
 
+console.log(`Opening save directory ${path}`);
+const world = new World(path);
+const dim = world.overworld;
+console.log(`Getting the block at x=${x} y=${y} z=${z}`);
 console.log(await dim.getBlock(x,y,z));
 
