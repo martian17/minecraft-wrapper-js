45e1d029a5a03b7ffc6eb26bac3e1493d9338a8c
martian17 <yakumodanshi@gmail.com>
2023-04-10 21:54:45 +0900

    Update to conform to the new cached API

--- tools/displayChunkPalette.mjs
+++ tools/displayChunkPalette.mjs
@@ -1,7 +1,7 @@
 import {World} from "../index.mjs";
 
 const path = process.argv[2];
-const [x=8,y=-61,z=9] = process.argv.slice(3).map(v=>parseInt(v));
+const [x=0,y=-64,z=0] = process.argv.slice(3).map(v=>parseInt(v));
 
 if(!path){
     console.log("please specify a path to the save data");
@@ -11,19 +11,11 @@ if(!path){
 const world = new World(path);
 const dim = world.overworld;
 
-console.log(x,z);
+console.log(`Getting the section at x=${x} y=${y} z=${z}`);
 
 const region = await dim.getRegion(x,z);
 console.log(region.path);
 const chunk = await dim.getChunk(x,z);
-console.log(chunk.id);
-//console.log(chunk.nbt.sections.map(s=>s.block_states));
-console.log(chunk.sections[0]?.nbt?.block_states?.palette);
-console.log(chunk.sections[1]?.nbt?.block_states?.palette);
-console.log(chunk.sections[2]?.nbt?.block_states?.palette);
-
-console.log(chunk.nbt);
-console.log(chunk.nbt.block_entities);
-
+console.log(chunk.getSection(y).nbt.block_states.palette);
 
 
