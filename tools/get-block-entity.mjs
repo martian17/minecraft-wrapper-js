99d54e2949916098fa481ebb333d2a4d1a6e59c1
martian17 <yakumodanshi@gmail.com>
2023-04-17 08:29:48 +0900

    Implement block state getter

---
+++ tools/get-block-entity.mjs
@@ -0,0 +1,21 @@
+import {World} from "../lib/index.mjs";
+import {promises as fs} from "fs";
+import {execSync} from "child_process";
+import {encodeRNBT, decodeRNBT} from "rnbt";
+import {encodeNBT, decodeNBT, NBT_Long} from "nbt.js";
+
+
+let [savedir, x=0, y=-64, z=0] = process.argv.slice(2);
+[x,y,z] = [x,y,z].map(v=>parseInt(v));
+
+if(!savedir){
+    console.log("please specify a path to the save data");
+    process.exit();
+}
+
+console.log(`Opening save directory ${savedir}`);
+const world = new World(savedir);
+const dim = world.overworld;
+console.log(`Getting the block entity at x=${x} y=${y} z=${z}`);
+console.log((await dim.getChunk(x,z)).blockEntities.get(x,y,z));
+
